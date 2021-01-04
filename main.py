c097752ed9ab7268b2bd1ea789043ef9b844b585
fedosovmu <fedosovmu@gmail.com>
2021-01-04 17:55:36 +0500

    Добавлен прототип экрана с лицензионным соглашением

--- main.py
+++ main.py
@@ -1,21 +1,37 @@
 import config
 config.set_config()
 from kivymd.app import MDApp
-from kivy.uix.button import Button
+from kivymd.uix.button import MDFlatButton
 from kivymd.uix.label import MDLabel
 from kivy.lang import Builder
 
 KV = """
 Screen:
-    MDRaisedButton:
-        text: "primary_light"
-        pos_hint: {"center_x": 0.5, "center_y": 0.7}
+    GridLayout:
+        rows: 2
+        
+        AnchorLayout:
+            anchor_x: 'left'
+            anchor_y: 'top'
+                             
+            Image:
+                source: 'data/images/360x270.png'
+                adaptive_width: True
+        
+        AnchorLayout:
+            anchor_x: 'center'
+            anchor_y: 'center'
+        
+            MDRaisedButton:
+                id: license_agreement_button
+                text: 'Принимаю'
+                pos_hint: {'center_x': 0.5, 'center_y': 0.7}
 """
 
 class MindTrackerApp(MDApp):
     def build(self):
-        screen = Builder.load_string(KV)
-        return screen
+        self.theme_cls.theme_style = "Dark"
+        return Builder.load_string(KV)
 
 
 if __name__ == '__main__':
