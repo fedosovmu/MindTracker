85a40f391567bc1c23e1212cd304ce06a01d7818
fedosovmu <fedosovmu@gmail.com>
2021-01-04 16:45:34 +0500

    kivy test app

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
+from kivy.app import App
+from kivy.uix.button import Button
 
-print('Hello world')
+class TestApp(App):
+    def build(self):
+        return Button(text='Hello World')
+
+TestApp().run()
