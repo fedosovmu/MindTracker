88ebd909a2bea4abee804d36a9a0daeb1e21836d
fedosovmu <fedosovmu@gmail.com>
2021-01-04 17:04:51 +0500

    KIvyMD test app

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
-from kivy.app import App
-from kivy.uix.button import Button
 import config
+config.set_config()
+from kivymd.app import MDApp
+from kivy.uix.button import Button
+from kivymd.uix.label import MDLabel
+from kivy.lang import Builder
 
+KV = """
+Screen:
+    MDRaisedButton:
+        text: "primary_light"
+        pos_hint: {"center_x": 0.5, "center_y": 0.7}
+"""
 
-class MindTrackerApp(App):
+class MindTrackerApp(MDApp):
     def build(self):
-        return Button(text='Hello World')
+        screen = Builder.load_string(KV)
+        return screen
 
 
 if __name__ == '__main__':
-    config.set_config()
     MindTrackerApp().run()
