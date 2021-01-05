511454e2f477c45b42150cddd65af86fcbb9fa32
fedosovmu <fedosovmu@gmail.com>
2021-01-05 22:07:50 +0500

    Добавлен класс с цветами приложения

---
+++ app_colors.py
@@ -0,0 +1,14 @@
+from kivy.utils import get_color_from_hex as hex
+
+
+class AppColors:
+    Dark = hex('#161617')
+    MiniDark = hex('#242426')
+    MainColor = hex('#229CF4')
+    SilverWhite = hex('#E3E7EA')
+    White = hex('#FFFFFF')
+
+    ImageLable = hex('#A3A3A3')
+    ImageBackground = hex('#2C2C2C')
+
+    PressedButton = hex('#afd3ed')
