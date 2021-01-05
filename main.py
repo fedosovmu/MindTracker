60351da0e266368e9f5f3d688896f5cfb5214ecc
fedosovmu <fedosovmu@gmail.com>
2021-01-05 15:35:48 +0500

    Рефакторинг, код экрана вынесен в отдельный файл

--- main.py
+++ main.py
@@ -4,41 +4,14 @@ from kivymd.app import MDApp
 from kivymd.uix.button import MDFlatButton
 from kivymd.uix.label import MDLabel
 from kivy.lang import Builder
+from kivy.uix.screenmanager import Screen, ScreenManager
 
-KV = """
-Screen:
-    BoxLayout:
-        orientation: 'vertical'
-                                        
-        Image:
-            source: 'data/images/360x270.png'
-            allow_stretch: True
-            keep_ratio: False
-            height: dp(270)
-            size_hint: (1, None)
-   
-        Label:
-            text: 'Для использования приложения вы\\nдолжны принять пользовательское\\nсоглашение'
-            multiline: True
-            width: dp(300)
-         
-        Button:
-            id: license_agreement_button
-            text: 'Принимаю'
-            height: dp(70)
-            size_hint: (1, None)
-            
-        Button:
-            id: license_agreement_button
-            text: 'Не принимаю'
-            height: dp(70)
-            size_hint: (1, None)
-"""
 
 class MindTrackerApp(MDApp):
     def build(self):
         self.theme_cls.theme_style = "Dark"
-        return Builder.load_string(KV)
+        Builder.load_file('mind_tracker.kv')
+        return Builder.load_file('mind_tracker.kv')
 
 
 if __name__ == '__main__':
