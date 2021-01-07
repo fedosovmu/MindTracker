91d1021fa32528176e16a4bc80c2efb87725a757
fedosovmu <fedosovmu@gmail.com>
2021-01-07 21:16:53 +0500

    Экран оценки настроения завершон (Но есть небольшие отличия от оригинального дизайна)

--- app_colors.py
+++ app_colors.py
@@ -8,6 +8,16 @@ class AppColors:
     SilverWhite = hex('#E3E7EA')
     White = hex('#FFFFFF')
 
+    Mood = {
+        '1': hex('#D63E3E'),
+        '2': hex('#F19914'),
+        '3': hex('#F5FA10'),
+        '4': hex('#3E8DD6'),
+        '5': hex('#10F1FF'),
+        '6': hex('#00FF66')
+    }
+
     ImageLable = hex('#A3A3A3')
     ImageBackground = hex('#2C2C2C')
-    PressedButton = hex('#AFD3ED')
+    MainButtonPressed = hex('#AFD3ED')
+    MoodButtonPressed = hex('#5a5a5a')
