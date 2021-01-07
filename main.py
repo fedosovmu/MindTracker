870e6d3c26496b60beba922626e78b039f0975b9
fedosovmu <fedosovmu@gmail.com>
2021-01-07 21:50:45 +0500

    Добавлено переключение между двумя экранами

--- main.py
+++ main.py
@@ -7,7 +7,12 @@ from kivy.uix.screenmanager import Screen, ScreenManager
 
 class MindTrackerApp(App):
     def build(self):
-        return Builder.load_file('kv_lang/mind_tracker.kv')
+        self.screen_manager = Builder.load_file('kv_lang/app.kv')
+        return self.screen_manager
+
+    def switch_screen(self, screen_name, direction='left'):
+        self.screen_manager.transition.direction = direction
+        self.screen_manager.current = screen_name
 
 
 if __name__ == '__main__':
