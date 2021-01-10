08d86baf8568e2befaed40adbf59324fab92688e
fedosovmu <fedosovmu@gmail.com>
2021-01-11 01:59:33 +0500

    Добавлена смена цветов шара и кнопки

--- main.py
+++ main.py
@@ -14,20 +14,23 @@ class MindTrackerApp(App):
         self.colors = AppColors
         Window.clearcolor = AppColors.app_background_color
         self.screen_manager = Builder.load_file('kv_lang/app.kv')
-        self.set_binding()
+        self.set_bindings()
         return self.screen_manager
 
     def switch_screen(self, screen_name, direction='left'):
         self.screen_manager.transition.direction = direction
         self.screen_manager.current = screen_name
 
-    def set_binding(self):
+    def set_bindings(self):
         mood_assessor_slider = self.screen_manager.ids.mood_assessment_screen.ids.mood_assessor.ids.mood_assessor_slider
-        mood_assessor_slider.fbind('value', self.mood_assessor_slider_on_value)
-        print(mood_assessor_slider)
+        mood_assessor_slider.fbind('value', self.mood_assessor_slider_on_value_changed)
 
-    def mood_assessor_slider_on_value(self, instance, value):
+    def mood_assessor_slider_on_value_changed(self, instance, value):
         print(f'Mood assessment changed {value}')
+        mood_sphere_image = self.screen_manager.ids.mood_assessment_screen.ids.mood_assessor.ids.mood_sphere_image
+        mood_sphere_image.source = f'data/images/mood_spheres/{value}.png'
+        mood_agree_button = self.screen_manager.ids.mood_assessment_screen.ids.mood_agree_button
+        mood_agree_button.current_mood = value
 
 if __name__ == '__main__':
     MindTrackerApp().run()
