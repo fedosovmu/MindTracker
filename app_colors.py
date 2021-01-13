a545d789ce29daa3e8cce3a599b757f5751c5e29
fedosovmu <fedosovmu@gmail.com>
2021-01-14 00:53:20 +0500

    Немного изменил длинну слайдера

--- app_colors.py
+++ app_colors.py
@@ -52,4 +52,4 @@ class AppColors:
     mood_assessor_secondary_text_color = design_colors['Purple/TextSecondary']
 
     mood_assessor_slider_cursor_colors = mood_colors.copy()
-    mood_assessor_slider_cursor_colors['1'] = design_colors['Black']
+    mood_assessor_slider_cursor_colors['1'] = hex('1D142A')
