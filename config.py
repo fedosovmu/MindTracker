fe28ceac65cc9a99efa71f3a87f443dfecf926da
fedosovmu <fedosovmu@gmail.com>
2021-01-16 23:50:05 +0500

    Config set exit on escape false

--- config.py
+++ config.py
@@ -5,3 +5,4 @@ def set_config():
     Config.set('graphics', 'width', '360')
     #Config.set('graphics', 'height', '640')
     Config.set('graphics', 'height', '568')
+    Config.set('kivy', 'exit_on_escape', '0')
